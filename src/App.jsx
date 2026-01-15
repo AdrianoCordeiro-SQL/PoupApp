9b3d0e2900668a8cf5f846e17fbe61c8dce4ec42
Adriano Cordeiro <adrianocordeiro99@outlook.com>
2026-01-15 09:49:32 -0300

    desenvolve e estiliza a tipografia

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,26 @@
-import { Aside } from './components/Aside'
-import { Container } from './components/container'
-import { Main } from './components/Main'
-import { Searchinput } from './components/SearchInput'
+import { Aside } from "./components/Aside";
+import { Container } from "./components/container";
+import { Main } from "./components/Main";
+import { Searchinput } from "./components/SearchInput";
+import { Typography } from "./components/Typography";
 function App() {
   return (
     <Container>
       <Aside />
       <Main>
         <Searchinput />
+        <div>
+          <Typography variant="h1">
+            Olá, Vinny!
+          </Typography>
+
+          <Typography variant="body">
+            Veja como estão suas finanças hoje.
+          </Typography>
+        </div>
       </Main>
     </Container>
-  )
+  );
 }
 
-export default App
+export default App;
