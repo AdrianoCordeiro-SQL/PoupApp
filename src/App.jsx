fc34096f8788cfd97f763d9a1382d6e23ecc1e93
Adriano Cordeiro <adrianocordeiro99@outlook.com>
2026-01-15 11:22:12 -0300

    estiliza cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
+import Card from "./components/Card";
+
 import { Aside } from "./components/Aside";
 import { Container } from "./components/container";
 import { Main } from "./components/Main";
 import { Searchinput } from "./components/SearchInput";
 import { Typography } from "./components/Typography";
+import { DailyBudget } from "./components/DailyBudget";
+
+
 function App() {
   return (
     <Container>
@@ -10,14 +15,36 @@ function App() {
       <Main>
         <Searchinput />
         <div>
-          <Typography variant="h1">
-            Olá, Vinny!
-          </Typography>
+          <Typography variant="h1">Olá, Vinny!</Typography>
 
           <Typography variant="body">
             Veja como estão suas finanças hoje.
           </Typography>
         </div>
+
+        <section>
+          <Card>
+            <Card.Header>Orçamento diário disponível:</Card.Header>
+            <Card.Body>
+              <DailyBudget value={250}/>
+            </Card.Body>
+          </Card>
+
+          <Card>
+            <Card.Header>Progresso da meta financeira</Card.Header>
+            <Card.Body>R$ 200</Card.Body>
+          </Card>
+
+          <Card>
+            <Card.Header>Movimentação financeira</Card.Header>
+            <Card.Body>R$ 200</Card.Body>
+          </Card>
+
+          <Card>
+            <Card.Header>Minhas contas</Card.Header>
+            <Card.Body>R$ 200</Card.Body>
+          </Card>
+        </section>
       </Main>
     </Container>
   );
